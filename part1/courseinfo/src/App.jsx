76181323cdaaca74dbdeae85d55edcab69e6f2b1
Thu Hoang <20014091+minhthuhoang@users.noreply.github.com>
2026-06-05 21:49:08 +1000

    add exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,29 @@
+const course = "Half Stack application development"
+const part1 = {part: "Fundamentals of React", exercises: 10}
+const part2 = {part: "Using props to pass data", exercises: 7}
+const part3 = {part: "State of a component", exercises: 14}
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
-const Content = ({part}) => {
+const Part = ({part}) => {
   return (
-    <p>
-      {part.part} {part.exercises}
+    <p> 
+      {part.part} {part.exercises} 
     </p>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part part= {part1} />
+      <Part part= {part2} />
+      <Part part= {part3} />
+    </div>
   )
 } 
 
@@ -19,16 +34,10 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = {part: "Fundamentals of React", exercises: 10}
-  const part2 = {part: "Using props to pass data", exercises: 7}
-  const part3 = {part: "State of a component", exercises: 14}
   return (
     <div>
       <Header course = {course} />
-      <Content part = {part1}  />
-      <Content part = {part2} />
-      <Content part = {part3} />
+      <Content />
       <Total total = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
